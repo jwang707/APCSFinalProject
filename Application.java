fe9e87ee3866ea27c827905b6d5f09a6c7588a21
Nour <>
2021-05-24 13:20:26 -0400

    wrote word bank variables. Jasmine and I are currently working on coming up with the terms for these word bank arrays, we're drafting that on the proposal doc and we'll add it to the class afterwards

--- Application.java
+++ Application.java
@@ -8,7 +8,11 @@ public class Application{
   private String[] ECs;
   private int college;
 
-//
+//Word Banks
+  String[][] intendedMajors; //come up with names for inteded intendedMajors
+  String[][] extraCurriuculars; //come up with names
+  String[] Statements; //come up with statements
+  String[] errorList = {"gpa", "sat", "IM", "EC", "statement"};
 
 
 }

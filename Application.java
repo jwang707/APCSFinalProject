a070af924423b347815e2df4592998dca8c4d88c
Nour <>
2021-05-24 13:14:36 -0400

    made class and wrote the variables for the application

---
+++ Application.java
@@ -0,0 +1,14 @@
+public class Application{
+
+
+//Application Variables
+  private String studentName, intendedMajor, statement;
+  private double gpa;
+  private int sat;
+  private String[] ECs;
+  private int college;
+
+//
+
+
+}
